bc5664946b78bebe45bb1ed20e758a3cfca009a4
Ducphan2k <119691110+Ducphan2k@users.noreply.github.com>
2023-07-16 22:43:08 +0700

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useEffect } from "react";
+
+const Home = ({ handleLogout }) => {
+  return (
+    <div className="container d-flex align-items-center text-center">
+      <div className="form-signin">
+        <h1 className="h3 mb-3 fw-normal">Welcome</h1>
+        <button
+          className="w-100 btn btn-lg btn-primary"
+          type="button"
+          onClick={handleLogout}
+        >
+          Log out
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
